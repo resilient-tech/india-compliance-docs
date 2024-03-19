236fc610663ad56ae1fdbfe17b9886c37a0d3a95
divyam-mistry <divsmistry30@gmail.com>
2024-03-19 15:20:49 +0530

    fix: minor styling changes

--- pages/.vitepress/theme/components/Step.vue
+++ pages/.vitepress/theme/components/Step.vue
@@ -10,10 +10,10 @@ defineProps({
       class="line absolute w-px h-[calc(100%-2.75rem)] top-[2.25rem] left-[0.9rem] bg-gray-200 dark:bg-[color:var(--vp-c-bg-soft-lighter)]">
     </div>
     <div class="w-full overflow-hidden pl-12 pr-px">
-      <h4 class="font-semibold text-gray-900 dark:text-gray-200">
+      <h4 class="font-medium text-[color:var(--vp-c-text-1)]">
         {{ title || "Default Title" }}
       </h4>
-      <div class="py-4 text-[color:var(--vp-c-text-2)]">
+      <div class="text-[color:var(--vp-c-text-2)]">
         <slot> <i>No description provided.</i> </slot>
       </div>
     </div>
